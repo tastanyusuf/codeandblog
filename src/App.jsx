c68bf5658581e5bd21d0db0cdcf6bd31643bcd7c
yusuf <tastan.yusuf@proton.me>
2025-06-15 21:50:18 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+  const head = "Code and Blog";
+
+  return (
+    <>
+      <div>
+        <h1>{head}</h1>
+      </div>
+      <div>
+        <a href="mailto:support@codeand.blog">support@codeand.blog</a>
+      </div>
+    </>
+  )
+}
+
+export default App
